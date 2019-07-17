95a3a0c890a11b8d3e046c7bdf4b9706c2b4a191
sjransom <sjransom@gmail.com>
2019-07-17 11:03:08 +0100

    setting up pages and basic layout

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+      <p>Reach me via <a href="https://twitter.com/sjransom" target="_blank" rel="noopener noreferrer">@sjransom</a> on Twitter.</p>
+    </Layout>
+  )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => <div>sjransom</div>
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+      <h2>I'm Sam, a front-end developer living in London</h2>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+    </Layout>
+  )
+}
+
+export default IndexPage
